a2841d44bb602391525f53b0a9a7af8411893901
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-06-17 11:32:48 +0300

    first commit 3

--- funcs.py
+++ funcs.py
@@ -1,6 +1,12 @@
-def add_numbers(a, b):
-    return a + b
+def add_numbers():
+    number_1 = input("Введите первое число: ")
+    number_2 = input("Введите второе число: ")
+    result = number_1 + number_2
+    print("Результат сложения:", result)
 
 
-def subtract_numbers(a, b):
-    return a - b
+def subtract_numbers():
+    number_1 = input("Введите первое число: ")
+    number_2 = input("Введите второе число: ")
+    result = number_1 - number_2
+    print("Результат сложения:", result)
